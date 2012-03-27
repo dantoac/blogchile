bce6ffbaf2e0a7d69b1a9ee40bb0ea582aca4001
Daniel Aguayo <dantoac@lavabit.com>
2012-03-27 17:07:06 -0300

    limpiando un poco

--- models/db.py
+++ models/db.py
@@ -1,42 +1,6 @@
-#'sqlite://storage2.sqlite' -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-if 0:
-    from gluon.sql import *
-    from gluon.validators import *
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-#########################################################################
+#-*- coding: utf-8 -*-
 
-if request.env.web2py_runtime_gae:            # if running on Google App Engine
-    db = DAL('gae')                           # connect to Google BigTable
-                                              # optional DAL('gae://namespace')
-    session.connect(request, response, db = db) # and store sessions and tickets there
-    ### or use the following lines to store sessions in Memcache
-    # from gluon.contrib.memdb import MEMDB
-    # from google.appengine.api.memcache import Client
-    # session.connect(request, response, db = MEMDB(Client()))
-else:                                         # else use a normal relational database
-    #db = DAL(['mysql://danto_bfra:040e10f4@localhost/danto_bfra','sqlite://storage.sqlite'])       # if not, use SQLite or other DB
-    db = DAL(['sqlite://database.sqlite'])
-    '''
-    acciones_readonly = ['feed']
-    if request.controller != 'noticias':
-        db = DAL(['sqlite://readonly.sqlite', 'postgres://danto:password@pass/danto_planet'])
-    else:
-        db = DAL(['sqlite://storage2.sqlite'])
-    '''
-## if no need for session
-#session.forget()
-
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
+db = DAL(['sqlite://database.sqlite'])
 
 from gluon.tools import *
 mail = Mail()                                  # mailer
@@ -101,9 +65,6 @@ tabla = db.define_table
 tabla('categoria',
     Field('title', label = T('Nombre')),
     Field('slug', compute = lambda r:IS_SLUG.urlify(r['title'])),
-    #Field('super', 'reference categoria', comment = 'SIN EFECTO todavía. Permitirá la jerarquización de categorías.'),
-    #Field('freq', 'integer', requires = IS_INT_IN_RANGE(300, 604801, error_message = 'conozco desde 300 hasta 604800 '), label = T('Frecuencia de actualización'), comment = 'SIN USO AÚN'),
-#    Field('actualizado', 'datetime', default = request.now, writable = False, readable = False),
     Field('description', 'text', label = T('Descripción'),comment=T('(opcional)')),
     auth.signature,
     format = '%(title)s',
@@ -130,18 +91,8 @@ db.feed.link.requires = IS_NOT_IN_DB(db, 'feed.link', error_message = T('Esta UR
 db.feed.title.requires = IS_NOT_IN_DB(db, 'feed.title', error_message = T('Ya hay otro feed con este título'))
 db.feed.categoria.requires = IS_IN_DB(db, 'categoria.id', '%(title)s', error_message = T('Debe caregorizar el feed para que pueda ser mostrado.'))
 
-
-### SIN USAR, aún
-tabla('feed_categoria',
-    Field('feed', db.feed),
-    Field('categoria', db.categoria, default = 1),
-    auth.signature,
-    )
-
-### requerimientos tabla feed_caregoría
-db.feed_categoria.is_active.writable = False
 #****************** verificando que no se repita la relación feed_categoría
-db.feed_categoria.feed.requires = IS_IN_DB(db, 'feed.id', '%(title)s', _and = IS_NOT_IN_DB(db(db.feed_categoria.categoria == request.vars.categoria), 'feed_categoria.feed', error_message = 'Este feed ya está asociado a esta categoría.'))
+#db.feed_categoria.feed.requires = IS_IN_DB(db, 'feed.id', '%(title)s', _and = IS_NOT_IN_DB(db(db.feed_categoria.categoria == request.vars.categoria), 'feed_categoria.feed', error_message = 'Este feed ya está asociado a esta categoría.'))
 
 
 tabla('noticia',
@@ -155,14 +106,6 @@ tabla('noticia',
       auth.signature,
       format = '%(slug)s',
       )
-
-'''
-tabla('votos_disponibles',
-      Field('user','reference auth_user'),
-      Field('feed_votados','list:reference feed'),
-      Field('votos_disponibles','integer',default=3)
-      )
-'''
 
 #response.generic_patterns = ['*']
 
