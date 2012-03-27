ddb4f7c5445591d00fc636d077509c424b8414c9
Daniel Aguayo <dantoac@lavabit.com>
2012-03-27 15:12:49 -0300

    commentando google-aplu-key en models/glovars.py

--- models/glovars.py
+++ models/glovars.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-google_api_key = 'AIzaSyCrrBUnAZx2IEpYYqyLuDYbyQ2l5J_GqOI'
+#google_api_key = 'AIzaSyCrrBUnAZx2IEpYYqyLuDYbyQ2l5J_GqOI'
 
 import os
 os.environ['TZ']='America/Santiago'
