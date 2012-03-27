1879fd943ecc24f3c10c38c86a5b5be8203d726e
Daniel Aguayo <dantoac@lavabit.com>
2012-03-27 17:11:42 -0300

    limpieza rápida

--- private/actualiza_feeds.py
+++ private/actualiza_feeds.py
@@ -15,7 +15,7 @@ import time
 
 import os
 os.environ['TZ']='America/Santiago'
-
+from random import choice
 #session.forget()
 
 xurl_service = ['http://go.gnu.cl/api.php?url',
@@ -25,73 +25,77 @@ xurl_service = ['http://go.gnu.cl/api.php?url',
             'http://is.gd/create.php?format=simple&url']
 
 
-def _u2d(fidx):
+def _u2d(feedlink):
     import gluon.contrib.feedparser as feedparser
     import urllib2
-    from random import choice
-    try:
-        feed = feedparser.parse(db.feed[fidx].link)
+    
+
+    fidx = feedlink[0]
+    flink = feedlink[1]
+
+    
+    feed = feedparser.parse(flink)
+
+    maxfeeds = 4
+    limite = 0
+
+    #print('%s: %s' % (request.now.now(),db.feed[fidx].title)) ###################### !
+    for e in feed.entries:
+        # revisando si el artículo obtenido ya estaba en la db
+        #edata = db((db.noticia.feed == fidx) & (db.noticia.title == XML(e.title))).select(db.noticia.id)
+
+        if limite == maxfeeds:
+            break
+
+        try:
+            xurl_api = choice(xurl_service)
+            xurl = urllib2.urlopen("%(api)s=%(longurl)s" % dict(api=xurl_api,longurl=e.link)).read()
+        except Exception,e:
+            print('No se pudo acortar la url: %s' % e)
+            continue
+
         
-        maxfeeds = 4
-        limite = 0
+        #last8news = db(db.noticia).count() - 8
+        no_existe = db((db.noticia.title.contains(XML(e.title)))).isempty()
+        #si no encuentra nada, inserta en la db, sino no hace nada
+        if no_existe:
+            
+            print('\t%s' % xurl)
 
-        #print('%s: %s' % (request.now.now(),db.feed[fidx].title)) ###################### !
-        for e in feed.entries:
-            # revisando si el artículo obtenido ya estaba en la db
-            edata = db((db.noticia.feed == fidx) & (db.noticia.title == XML(e.title))).select(db.noticia.id)
+            try:
+                actualizado=e.updated
+            except:
+                actualizado=request.now.now()
 
-            if limite == maxfeeds:
-                break
+            try:
+                DESCRIPTION = e.description
+            except:
+                DESCRIPTION = e.link
 
-            #si no encuentra nada, inserta en la db, sino no hace nada
-            if len(edata) == 0:
-                xurl_api = choice(xurl_service)
-
-                try:
-                    xurl = urllib2.urlopen("%(api)s=%(longurl)s" % dict(api=xurl_api,longurl=e.link)).read()
-                except:
-                    xurl = urllib2.urlopen("%(api)s=%(longurl)s" % dict(api=xurl_api,longurl=e.link)).read()
-
-                print('\t%s' % xurl)
-
-                try:
-                    actualizado=e.updated
-                except:
-                    actualizado=request.now.now()
-
-                #ddg="http://lmddgtfy.com/?q=%(term)s+%(sitio)s" % dict(term=XML(e.title),sitio=XML(db.feed[fidx].title))
-                #ddg = "http://lmddgtfy.com/?q=%(term)s+%(sitio)s" % dict(term=e.slug.replace('-',' '),sitio=XML(e.feed.title))
-                try:
-                    DESCRIPTION = e.description
-                except:
-                    DESCRIPTION = e.link
-
-                try:
-                    db.noticia.insert(title = XML(e.title), link = e.link, description = XML(DESCRIPTION),
-                                      updated = actualizado, created_on=request.now.now(), feed = fidx, shorturl=xurl)#, slug = slug)
-                    db.commit()
-                except:
-                    pass
-
+            try:
+                db.noticia.insert(title = XML(e.title), link = e.link, description = XML(DESCRIPTION),
+                                  updated = actualizado, created_on=request.now.now(), feed = fidx, shorturl=xurl)
+                db.commit()
+            except Exception,e:
+                print('Error registrando noticia: %s' % e)
             limite += 1
+        else:
+            print('Ya existe entre las últimas 8 noticias')
+            continue
         
-    except:
-        exit
-"""
-feed_data = db((db.feed.categoria == db.categoria.id)
-            #& (db.feed_categoria.feed == db.feed.id)
-            #& (db.feed_categoria.is_active == True)
-            & (db.feed.is_active == True)
-            & (db.categoria.is_active == True)
-            ).select(db.feed.id)
-"""
 
-feed_data = db((db.feed.id>0) & (db.feed.is_active==True)).select(db.feed.id)
+'''
+feed_data = db((db.feed.id>0) & 
+           (db.feed.is_active==True) &
+           (db.feed.categoria < 3)
+           ).select(db.feed.id)
+'''
+links = [(f.id,f.link) for f in db((db.feed.id>0) & 
+                                   (db.feed.is_active==True)).select()]
 
 #try:
-for feed in feed_data:
-    fidx = feed.id
-    _u2d(fidx)
-        #time.sleep(3) #en segundos
-#except Exception,e:
-#    print "excepcion: %s" % e
+for feedlink in links:
+    
+    _u2d(feedlink)    
+        #except Exception,e:
+        #    print "Excepción: %s" % e
